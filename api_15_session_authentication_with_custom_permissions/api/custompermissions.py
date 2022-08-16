1f1586517f9775619bc08755fed419fd862d9eba
Nitin Kumar <70315471+monty533@users.noreply.github.com>
2022-08-16 11:14:21 +0530

    Delete api_15_session_authentication_with_custom_permissions directory

--- api_15_session_authentication_with_custom_permissions/api/custompermissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class MyPermission(BasePermission):
-    def has_permission(self, request, views):
-        if request.method == "POST":
-            return True
-        return False
